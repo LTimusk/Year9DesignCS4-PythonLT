b8f53ba4ddf2073c1300a3027b240c99dadfcb27
Lukas Timusk <lukas.timusk@Y22-Design-Lukas-Timusk.local>
2018-09-26 10:33:45 -0400

    CylinderVolGui

---
+++ AreaCalculator.py
@@ -0,0 +1,27 @@
+
+import os
+
+#Input
+
+print("****Area Calculator****")
+
+os.system("say hey boi how u doing welcome to my area calcuator")
+
+os.system("say what is your length")
+length = input("Input length: ")
+length = int(length)
+
+os.system("say what is your width") 
+width = input("Input width: ")
+width = int(width)
+
+#Process
+
+area = length*width
+
+#Output
+
+print("Your area is: "+str(area))
+
+os.system("say thanks for using my calculator my g")
+
---
+++ SaySomething.py
@@ -0,0 +1,8 @@
+
+import os
+
+
+statement = input ("what would you like to eat? : ")
+os.system("say "+statement)
+
+print("End of terminal")
