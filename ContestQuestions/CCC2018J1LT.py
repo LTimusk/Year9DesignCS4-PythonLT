7e1ceea8f78907c0aa8be2e252670c0c23515c79
Lukas Timusk <lukas.timusk@Y22-Design-Lukas-Timusk.local>
2018-10-15 11:28:03 -0400

    Contest Questions Updated

---
+++ ContestQuestions/CCC2018J1LT.py
@@ -0,0 +1,23 @@
+#Imput 
+
+
+a = input("What is the first digit: ")
+a = int(a)
+
+b = input("What is the second digit: ")
+b = int(b)
+
+c = input("What is the third digit: ")
+c = int(c)
+
+d= int(input("What is the fourth digit: "))
+
+
+
+#Process 
+
+
+
+
+
+#Output 
