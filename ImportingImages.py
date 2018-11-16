651103574f4dd04e1a00a987e84251660b1d9749
Lukas Timusk <lukas.timusk@Y22-Design-Lukas-Timusk.local>
2018-11-16 15:28:01 -0500

    PressingImages.py

---
+++ ImportingImages.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+
+photo = 
+
+
+
+
+root.mainloop()
---
+++ PressingImages.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+from PIL import ImageTk, Image
+#To start this program we have used the program GUIAddingJPG.py
+#to setup the image for clicking. 
+
+def clicked1(event):
+	print("Clicked - 1")
+
+
+def clicked2(event):
+	print("Clicked - 2")
+
+#This creates the main window of an application
+window = tk.Tk()
+window.title("Join")
+window.geometry("600x780")
+window.configure(background='grey')
+
+path = "VolleyballTrackerLT/100_Fermium.jpg"
+
+#Creates a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object.
+img = ImageTk.PhotoImage(Image.open(path))
+
+#The Label widget is a standard Tkinter widget used to display a text or image on the screen.
+panel = tk.Label(window, image = img)
+
+#The Pack geometry manager packs widgets in rows or columns.
+panel.pack(side = "bottom", fill = "both", expand = "yes")
+panel.bind("<Button-1>",clicked1)
+panel.bind("<Button-2>",clicked2)
+
+#Start the GUI
+window.mainloop()
